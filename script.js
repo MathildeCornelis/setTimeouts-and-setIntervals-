339650180855af6afc127396b569f52d385e4d44
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-12 14:05:13 +0100

    exercice 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Exercise 1*/
+/*Exercise 1
 let prout = ['p', 'r', 'o', 'u', 't'];
 let myInterval = setInterval(afficherProut, 1000); 
 let i = 0;
@@ -6,12 +6,33 @@ let i = 0;
 
 function afficherProut() {
     if (i < prout.length){
-        document.getElementById('demo').innerHTML += (prout[i]);
+        document.getElementById('prout').innerHTML += (prout[i]);
         i++;
     }else if (i == prout.length){
-        document.getElementById('demo').textContent = "";
+        document.getElementById('prout').textContent = "";
         i=0;
     };
     setTimeout(afficherProut, speed);
 };
 window.addEventListener("load", afficherProut);
+*/
+
+/*Exercise 2*/
+
+let seconds = 0;
+
+function addSeconds() {
+    seconds += 1;
+    document.getElementById('second-counter').innerText = "You have been here for " + seconds + " seconds.";
+};
+
+let setIntervalSeconds = setInterval(addSeconds, 1000);
+
+let minutes = 0;
+
+function addMinutes() {
+    minutes += 1;
+    document.getElementById('minute-counter').innerText = minutes + " minute" + (minutes > 1 ? "s" : "") + " have passed.";
+}
+
+let setIntervalMinutes = setInterval(addMinutes, 60000);
