10b6beeb66b16afa029c2e436c570fa760ac3150
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-12 14:11:33 +0100

    exercice 2

--- ex 1 et 2/script.js
+++ ex 1 et 2/script.js
@@ -1,4 +1,4 @@
-/*Exercise 1
+/*Exercise 1*/
 let prout = ['p', 'r', 'o', 'u', 't'];
 let myInterval = setInterval(afficherProut, 1000); 
 let i = 0;
@@ -15,24 +15,19 @@ function afficherProut() {
     setTimeout(afficherProut, speed);
 };
 window.addEventListener("load", afficherProut);
-*/
 
 /*Exercise 2*/
 
 let seconds = 0;
-
 function addSeconds() {
     seconds += 1;
     document.getElementById('second-counter').innerText = "You have been here for " + seconds + " seconds.";
 };
-
 let setIntervalSeconds = setInterval(addSeconds, 1000);
 
 let minutes = 0;
-
 function addMinutes() {
     minutes += 1;
     document.getElementById('minute-counter').innerText = minutes + " minute" + (minutes > 1 ? "s" : "") + " have passed.";
 }
-
 let setIntervalMinutes = setInterval(addMinutes, 60000);
